b9b4a2c6df0f96c6023c04f324f0b5c3524426c3
Israel <olaoyeaisrael@gmail.com>
2025-10-29 14:46:04 +0100

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import Network from './components/Network'
-import Acheivement from './components/Acheivement'
-import Footer from './components/Footer'
+
 
 import Login from './_auth/login'
-import ForgetPassword from './_auth/forgetPassword'
+
 import SignUp from './_auth/signUp'
 import { Navigate, Route, Router, Routes } from 'react-router-dom'
-import Dashboard from './_root/pages/Home'
+
 import RootLayout from './_root/RootLayout'
 import TakeTest from './_root/pages/TakeTest'
 import CalculateCGPA from './_root/pages/CalculateCGPA'
 import Home from './_root/pages/Home'
-import Community from './_root/pages/community'
+import Community from './_root/pages/Community'
 import Schedule from './_root/pages/Schedule'
 import RootLayoutLand from './_root/RootLayoutLand'
 import Landing from './_root/landing'
@@ -25,8 +19,7 @@ import Academics from './_root/welcome/Academics'
 import About from './_root/welcome/About'
 import { Testimonial } from './_root/welcome/Testimonial'
 import Contact from './_root/Contact'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
+
 
 
 
